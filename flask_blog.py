a0af3c341c38c2c57ef3987a7b7df8cb2e9b3b5c
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-09-12 19:23:29 +0530

    flask setup

---
+++ flask_blog.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+  {
+    'author': 'Anuj Samdariya',
+    'title': 'JS Tutorial',
+    'content': 'First Post Content',
+    'date_posted': 'September 12, 2024',
+  },
+  {
+    'author': 'John Doe',
+    'title': 'TS Tutorial',
+    'content': 'Second Post Content',
+    'date_posted': 'September 14, 2024',
+  },
+]
+
+@app.route("/")
+def hello_world():
+  return "<p>Hello, World!</p>"
+
+@app.route("/home")
+def home():
+  return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+  return render_template('about.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
