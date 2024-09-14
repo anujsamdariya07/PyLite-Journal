3ecef02031aa10e35d970355434da1d43c071a9c
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-09-14 21:42:41 +0530

    pagination + post order + user page

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from flask_blog import app, db
-from flask_blog.models import User
+from flask_blog.models import User, Post
 
 with app.app_context():
-  db.create_all()
+  posts = Post.query.paginate(per_page=2, page=6)
+  # print(dir(posts))
+  # print(posts.total)
+  for post in posts.iter_pages():
+    print(post, end=' ')
